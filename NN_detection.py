51ff810183af7e685b5292b9d8388367a2645064
AnthonyKeung <anthonykeung2468@gmail.com>
2024-12-02 18:42:34 +0000

    Giving up detecting the exact location

--- NN_detection.py
+++ NN_detection.py
@@ -39,8 +39,50 @@ def peak_finding_training(windows: list, window_size: int,  peak_indices: list, 
 
     # Save the trained model
     model.save(model_save_path)
+def peak_finding_training_v2(windows: list, window_size: int,  peak_indices: list, model_save_path: str):
 
-def index_generator(windows, model_path):
+     # Creating all the data for the model
+    X = np.array(windows)
+    y = np.zeros((len(windows), window_size))
+
+    for peak in peak_indices:
+        i = peak // window_size # Integer division
+        j = peak % window_size # Modulus
+        y[i][j] = 1
+
+
+    # #Sanity check for good data in 
+    # print(f"The first 10 peaks is {peak_indices[:10]}")
+    # counter  = 0
+    # #check the one is in the right place  
+    # for i in range(len(y)):
+    #     for j in range(len(y[i])):
+    #         if y[i][j] == 1:
+    #             counter += 1
+    #             print(f"{i*window_size + j} is in the right place")
+    #             if counter == 10:
+    #                 return
+    
+
+   # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+ # Define the neural network model
+    model = Sequential([
+        LSTM(500, input_shape=(window_size, 1)),
+        Dense(200, activation='relu'),
+        Dense(window_size, activation='sigmoid')
+    ])
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    # Train the model using training data
+    model.fit(X_train, y_train, epochs=10, batch_size=100, validation_data=(X_test, y_test))
+
+    # Save the trained model
+    model.save(model_save_path)
+
+def index_generator(windows, window_size, model_path,save_path):
 
     # Convert the list of windows to a NumPy array and reshape it
     X_windows = np.array(windows).reshape(len(windows), window_size, 1)
@@ -56,10 +98,30 @@ def index_generator(windows, model_path):
         if prediction > 0.5:
             index.append(i*window_size)
 
-    spio.savemat('predicted_indices.mat', {'Index': index})
+    spio.savemat(save_path, {'Index': index})
+    return index
+def index_generator_v2(windows, window_size, model_path,save_path): 
+    
+    # Convert the list of windows to a NumPy array and reshape it
+    X_windows = np.array(windows).reshape(len(windows), window_size, 1)
+    # Load the trained model
+    model = tf.keras.models.load_model(model_path)
+
+    # Predict peaks in the test windows
+    predictions = model.predict(X_windows)
+    print(f"Predictions shape: {predictions.shape}")
+    print(f"Predictions: {predictions[:2]}")
+    index = []
+
+    for i, prediction in enumerate(predictions):
+        for j, _ in enumerate(prediction):
+            if prediction[j] > 0.5:
+                index.append((i*window_size) + j)
+
+    spio.savemat(save_path, {'Index': index})
     return index
 
-def class_generator(raw_data, peak_indices, model_path):
+def class_generator(raw_data, peak_indices, window_size, model_path, save_path):
 
     # Creating all the data for the model
     windows = []
@@ -76,14 +138,17 @@ def class_generator(raw_data, peak_indices, model_path):
     predictions = model.predict(X_windows)
     predictions = np.argmax(predictions, axis=1).tolist()
     predictions = [prediction + 1 for prediction in predictions]
-    print(predictions)
-    spio.savemat('predicted_class.mat', {'Class': predictions})
+    # Load existing .mat file if it exists
+    try:
+        existing_data = spio.loadmat(save_path, squeeze_me=True)
+        existing_data['Class'] = predictions
+        spio.savemat(save_path, existing_data)
+    except FileNotFoundError:
+        pass
     return predictions
 
 def window_generator(raw_data, window_size):
     windows = [raw_data[i:i + window_size] for i in range(0, len(raw_data), window_size)]
-    print(f"Number of windows: {len(windows)}")
-    print(f"There should be {len(raw_data) // window_size} windows")
     return windows
 
 def peak_type_training(raw_data, window_size, peak_types, peak_indices, model_save_path):
@@ -122,19 +187,31 @@ def peak_type_training(raw_data, window_size, peak_types, peak_indices, model_sa
     # Save the trained model
     model.save(model_save_path)
 
+def generate_mat_file(peak_detection_model_path, peak_types_model_path):
+    for dataset in ['D2', 'D3', 'D4', 'D5', 'D6']:
+        mat = spio.loadmat(f'{dataset}.mat', squeeze_me=True)
+        raw_data = mat['d']
+        window_size = 50
+        windows = window_generator(raw_data, window_size)
+
+        # peak_finding_training(windows, window_size, peak_indices, 'peak_detection_model.h5')
+        index_generator(windows, peak_detection_model_path,f'predicted/{dataset}.mat' )
+
+        predicted_mat = spio.loadmat(f'predicted/{dataset}.mat', squeeze_me=True)
+        peak_indices = predicted_mat['Index']
+
+        #peak_type_training(raw_data, window_size, peak_types, peak_indices, 'peak_type_model.h5')
+        class_generator(raw_data, peak_indices, window_size,  peak_types_model_path, f'predicted/{dataset}.mat')
+
 if __name__ == '__main__':
+    mat = spio.loadmat('D1.mat', squeeze_me=True)
+    d = mat['d']
+    Index = mat['Index']
+    Index = sorted(Index)
+    peak_type  = mat['Class']
 
-    mat = spio.loadmat('D2.mat', squeeze_me=True)
-    raw_data = mat['d']
-    peak_indices = mat['Index']
-    peak_types = mat['Class']
-    window_size = 50
+    window_size = 20
 
-    windows = window_generator(raw_data, window_size)
+    windows = window_generator(raw_data=d, window_size = window_size)
+    peak_finding_training_v2(windows, window_size, peak_indices=Index, model_save_path='peak_finding_model_v2.h5')
 
-    # peak_finding_training(windows, window_size, peak_indices, 'peak_detection_model.h5')
-    # index_generator(windows, 'peak_detection_model.h5')
-    # peak_type_training(raw_data, window_size, peak_types, peak_indices, 'peak_type_model.h5')
-    class_generator(raw_data, peak_indices, 'D2_peak_type_model.h5')
-
-    
--- initial_tests.py
+++ initial_tests.py
@@ -4,25 +4,39 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import scipy.io as spio
 import matplotlib.pyplot as plt
 
-mat = spio.loadmat('D1.mat', squeeze_me=True)
-mymat= spio.loadmat('predicted_class.mat', squeeze_me=True)
+import NN_detection as nn
 
+
+window_size = 200
+
+mat = spio.loadmat('D1.mat', squeeze_me=True)
 d = mat['d']
 Index = mat['Index']
+Index = sorted(Index)
 peak_type  = mat['Class']
 
-predicted_peak_type = mymat['Class']  
+windows = nn.window_generator(d, window_size)
 
-print(f"First 10 peaks: {peak_type[:10]}")  
-print(f"First 10 predicted peaks: {predicted_peak_type[:10]}")
+# nn.peak_finding_training_v2(windows, window_size, Index, 'peak_finding_model_v2.h5')
+predicted_indices = nn.index_generator_v2(windows, window_size, 'peak_finding_model_v2.h5', 'predicted_indices.mat')
 
-cm = confusion_matrix(peak_type, predicted_peak_type)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-disp.plot()
+print(f"The first 10 predicted indices are: {predicted_indices[:10]}")
+print(f"The first 10 actual indices are: {Index[:10]}")
 
+# nn.peak_type_training(d, window_size, peak_type, Index, 'peak_type_model.h5')
+# nn.class_generator(d, Index, window_size, 'peak_type_model.h5', 'predicted_class.mat')
+# mymat= spio.loadmat('predicted_class.mat', squeeze_me=True)
 
+# predicted_peak_type = mymat['Class']
 
-plt.show()
+# accuracy = (predicted_peak_type == peak_type).mean() * 100
+# print(f'Percentage correct: {accuracy:.2f}%')
+
+# cm = confusion_matrix(peak_type, predicted_peak_type)
+# disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+# disp.plot()
+# plt.show()
+
 # plt.plot(d)
 # plt.plot(Index, d[Index], "x", color="red")
 # plt.plot(predicted_indices, d[predicted_indices], "o", color="green")
