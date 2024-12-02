f328627d28527bbb25cecbee5c4da02ab67f02f6
AnthonyKeung <anthonykeung2468@gmail.com>
2024-12-02 00:59:48 +0000

    Both Class detection and Peak detection sorted Bosh

--- NN_detection.py
+++ NN_detection.py
@@ -4,92 +4,137 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM
 import numpy as np
 from sklearn.model_selection import train_test_split
-
-
 import scipy.io as spio
 import matplotlib.pyplot as plt
 
-# mat = spio.loadmat('D1.mat', squeeze_me=True)
-# raw_data = mat['d']
+def peak_finding_training(windows: list, window_size: int,  peak_indices: list, model_save_path: str):
 
-# peak_indices = mat['Index']
-# peak_indices = sorted(peak_indices)
+    # Creating all the data for the model
+    X = np.array(windows)
+    y = np.zeros((len(windows), 1))
 
-# #split raw data into window size of 100 
-# window_size = 100
-# windows = [raw_data[i:i + window_size] for i in range(0, len(raw_data), window_size)]
+    # Mark windows containing peaks
+    for peak in peak_indices:
+        for j, _ in enumerate(windows):
+            if j*window_size <= peak <= (j+1)*window_size:
+                y[j] = 1
+                break
 
-# print(f"Number of windows: {len(windows)}")
-# print(f"There should be {len(raw_data) // window_size} windows")
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# # Creating all the data for the model
-# X = np.array(windows)
-# y = np.zeros((len(windows), 1))
+    # Define the neural network model
+    model = Sequential([
+        LSTM(50, input_shape=(window_size, 1)),
+        Dense(1, activation='sigmoid')
+    ])
 
-# # Mark windows containing peaks
-# for peak in peak_indices:
-#     for j, window in enumerate(windows):
-#         if j*window_size <= peak <= (j+1)*window_size:
-#             y[j] = 1
-#             print(f"{peak} is inbetween {j*window_size} and {(j+1)*window_size}")
-#             break
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+    # Reshape X for LSTM layer
+    X = X.reshape((X.shape[0], X.shape[1], 1))
 
-            
+    # Train the model using training data
+    model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
 
-# # Print the number of 1s in disparity dues to multiple peaks in windows
-# print(f"Number of windows containing peaks: {np.sum(y)}")
-# print(f"Number of actual peaks: {len(peak_indices)}")
+    # Save the trained model
+    model.save(model_save_path)
 
-# # # Split the data into training and testing sets
-# # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def index_generator(windows, model_path):
 
+    # Convert the list of windows to a NumPy array and reshape it
+    X_windows = np.array(windows).reshape(len(windows), window_size, 1)
 
-# # # Define the neural network model
-# # model = Sequential([
-# #     LSTM(50, input_shape=(window_size, 1)),
-# #     Dense(1, activation='sigmoid')
-# # ])
+    # Load the trained model
+    model = tf.keras.models.load_model(model_path)
 
-# # model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    # Predict peaks in the test windows
+    predictions = model.predict(X_windows)
+    index = []
 
-# # # Reshape X for LSTM layer
-# # X = X.reshape((X.shape[0], X.shape[1], 1))
+    for i, prediction in enumerate(predictions):
+        if prediction > 0.5:
+            index.append(i*window_size)
 
-# # # Train the model using training data
-# # model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+    spio.savemat('predicted_indices.mat', {'Index': index})
+    return index
 
-# # # Save the trained model
-# # model.save('peak_detection_model.h5')
+def class_generator(raw_data, peak_indices, model_path):
 
+    # Creating all the data for the model
+    windows = []
+    for peak_index in peak_indices:
+        windows.append(raw_data[peak_index:peak_index + window_size])
 
-mat = spio.loadmat('D1.mat', squeeze_me=True)
-raw_data = mat['d'][:20000]
-actual_peak_indices = sorted(mat['Index'])
-actual_peak_indices = actual_peak_indices[:10]
-raw_data = raw_data
+    # Convert the list of windows to a NumPy array and reshape it
+    X_windows = np.array(windows).reshape(len(windows), window_size, 1)
 
-window_size = 50
-windows = [raw_data[i:i + window_size] for i in range(0, len(raw_data), window_size)]
-peak_count = 0 
+    # Load the trained model
+    model = tf.keras.models.load_model(model_path)
 
-# Predict peaks in the test windows
-model = tf.keras.models.load_model('peak_detection_model.h5')
-peak_count = 0
+    # Predict peaks in the test windows
+    predictions = model.predict(X_windows)
+    predictions = np.argmax(predictions, axis=1).tolist()
+    predictions = [prediction + 1 for prediction in predictions]
+    print(predictions)
+    spio.savemat('predicted_class.mat', {'Class': predictions})
+    return predictions
 
-for i, window in enumerate(windows):
-    prediction = model.predict(window.reshape(1, window_size, 1))
-    if prediction > 0.5:
-        plt.scatter(range(i*window_size, (i+1)*window_size), window, color='red', marker='x')
-        peak_count += 1
+def window_generator(raw_data, window_size):
+    windows = [raw_data[i:i + window_size] for i in range(0, len(raw_data), window_size)]
+    print(f"Number of windows: {len(windows)}")
+    print(f"There should be {len(raw_data) // window_size} windows")
+    return windows
 
-plt.plot(raw_data, label='Raw Data')
-plt.plot(actual_peak_indices, raw_data[actual_peak_indices], 'x', label='Actual Peaks', color='green')
+def peak_type_training(raw_data, window_size, peak_types, peak_indices, model_save_path):
 
-plt.title('Raw Data with Peaks')
-plt.xlabel('Sample Index')
-plt.ylabel('Amplitude')
-plt.legend()
-plt.show()
-print(f"Total number of peaks detected: {peak_count}")
+    # Creating all the data for the model
+    windows = []
+    for peak_index in peak_indices:
+        windows.append(raw_data[peak_index:peak_index + window_size])
+    
+    print(f"Number of windows: {len(windows)}")
 
+    X = np.array(windows)
+    y = np.array(peak_types)
+    y = y-1
+
+    # Convert peak types to one-hot encoding
+    y = tf.keras.utils.to_categorical(y, num_classes=5)
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Define the neural network model
+    model = Sequential([
+        LSTM(50, input_shape=(window_size, 1)),
+        Dense(5, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    # Reshape X for LSTM layer
+    X = X.reshape((X.shape[0], X.shape[1], 1))
+
+    # Train the model using training data
+    model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+
+    # Save the trained model
+    model.save(model_save_path)
+
+if __name__ == '__main__':
+
+    mat = spio.loadmat('D2.mat', squeeze_me=True)
+    raw_data = mat['d']
+    peak_indices = mat['Index']
+    peak_types = mat['Class']
+    window_size = 50
+
+    windows = window_generator(raw_data, window_size)
+
+    # peak_finding_training(windows, window_size, peak_indices, 'peak_detection_model.h5')
+    # index_generator(windows, 'peak_detection_model.h5')
+    # peak_type_training(raw_data, window_size, peak_types, peak_indices, 'peak_type_model.h5')
+    class_generator(raw_data, peak_indices, 'D2_peak_type_model.h5')
+
+    
--- initial_tests.py
+++ initial_tests.py
@@ -1,16 +1,31 @@
 from scipy.signal import find_peaks
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 import scipy.io as spio
 import matplotlib.pyplot as plt
 
 mat = spio.loadmat('D1.mat', squeeze_me=True)
+mymat= spio.loadmat('predicted_class.mat', squeeze_me=True)
+
 d = mat['d']
 Index = mat['Index']
-Index = Index[0:10]
+peak_type  = mat['Class']
 
-print(f"The index of the peaks are {Index}")
-print(f"The value of the peaks are {d[Index]}")
+predicted_peak_type = mymat['Class']  
 
-plt.plot(d)
-plt.plot(Index, d[Index], "x")
+print(f"First 10 peaks: {peak_type[:10]}")  
+print(f"First 10 predicted peaks: {predicted_peak_type[:10]}")
+
+cm = confusion_matrix(peak_type, predicted_peak_type)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot()
+
+
+
 plt.show()
+# plt.plot(d)
+# plt.plot(Index, d[Index], "x", color="red")
+# plt.plot(predicted_indices, d[predicted_indices], "o", color="green")
+# plt.legend(['Signal', 'Actual Peaks', 'Predicted Peaks'])
+
+# plt.show()
