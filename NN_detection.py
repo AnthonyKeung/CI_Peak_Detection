19b5e5adc977fe890974ccba5960ad85b4703da0
AnthonyKeung <anthonykeung2468@gmail.com>
2024-12-13 00:32:03 +0000

    Final workings corrected again

--- NN_detection.py
+++ NN_detection.py
@@ -111,7 +111,7 @@ def index_generator(windows, window_size, model_path,save_path):
     spio.savemat(save_path, {'Index': index})
     return index
 
-def class_training(raw_data, filtered_data, window_size, peak_types, peak_indices, model_save_path):
+def class_training(raw_data, filtered_data, window_size, peak_types, peak_indices, model_save_path,  show_graph=False):
 
     # Making sure the peaks are at the centre of the windows
     filtered_windows = []
@@ -122,14 +122,15 @@ def class_training(raw_data, filtered_data, window_size, peak_types, peak_indice
     for peak_index in peak_indices:
         raw_windows.append(raw_data[peak_index - 20 :peak_index + 30])
 
-    for i in range(5):
-        plt.subplot(5, 1, i+1)
-        random_index = np.random.randint(0, len(filtered_windows))
-        plt.plot(filtered_windows[random_index], 'g')
-        plt.plot(raw_windows[random_index], 'r')
-        plt.title(f'Window {random_index} Class {peak_types[random_index]}')
-    plt.tight_layout()
-    plt.show()
+    if show_graph:
+        for i in range(5):
+            plt.subplot(5, 1, i+1)
+            random_index = np.random.randint(0, len(filtered_windows))
+            plt.plot(filtered_windows[random_index], 'g')
+            plt.plot(raw_windows[random_index], 'r')
+            plt.title(f'Window {random_index} Class {peak_types[random_index]}')
+        plt.tight_layout()
+        plt.show()
     
     print(f"Number of windows: {len(filtered_windows)}")
 
@@ -160,7 +161,7 @@ def class_training(raw_data, filtered_data, window_size, peak_types, peak_indice
     # Save the trained model
     model.save(model_save_path)
 
-def class_generator(raw_data, filtered_data, peak_indices, window_size, model_path, save_path):
+def class_generator(raw_data, filtered_data, peak_indices, window_size, model_path, save_path, show_graph=False):
     """
     Generates class predictions for given data using a trained neural network model.
     Parameters:
@@ -170,6 +171,7 @@ def class_generator(raw_data, filtered_data, peak_indices, window_size, model_pa
     window_size (int): The size of the window to use for each peak.
     model_path (str): Path to the trained neural network model.
     save_path (str): Path to save the predictions in a .mat file.
+    show_graph (bool): Whether to display a graph of the data.
     Returns:
     list: A list of predicted classes for each window.
     """
@@ -193,22 +195,20 @@ def class_generator(raw_data, filtered_data, peak_indices, window_size, model_pa
 
     # Predict peaks in the test windows
     predictions = model.predict(X_windows)
-    for i in range(10):
-        i = np.random.randint(0, len(predictions))
-        print(f"Prediction {i}: {max(predictions[i])}")
     predictions = np.argmax(predictions, axis=1).tolist()
     predictions = [prediction + 1 for prediction in predictions]
 
     print(f"For {save_path} the number of predictions are {len(predictions)}")
 
-    for i in range(5):
-        plt.subplot(5, 1, i+1)
-        random_index = np.random.randint(0, len(windows))
-        plt.plot(windows[random_index])
-        plt.plot(raw_windows[random_index])
-        plt.title(f'Window {random_index} Class {predictions[random_index]}')
-    plt.tight_layout()
-    plt.show()
+    if show_graph:
+        for i in range(5):
+            plt.subplot(5, 1, i+1)
+            random_index = np.random.randint(0, len(windows))
+            plt.plot(windows[random_index])
+            plt.plot(raw_windows[random_index])
+            plt.title(f'Window {random_index} Class {predictions[random_index]}')
+        plt.tight_layout()
+        plt.show()
     # Load existing .mat file if it exists
     try:
         existing_data = spio.loadmat(save_path, squeeze_me=True)
@@ -218,11 +218,12 @@ def class_generator(raw_data, filtered_data, peak_indices, window_size, model_pa
         print(f"file not found ")
     return predictions
 
-def generate_mat_file(model_path):
+def generate_mat_file(model_path, show_graph=False):
     """
     Processes a dataset of .mat files, filters the data, detects peaks, and generates new .mat files with peak indices.
     Args:
         model_path (str): Path to the model used for class generation.
+        show_graph (bool): Whether to display graphs of the data.
     Dataset format:
         Each entry in the dataset is a list with the following elements:
             - params[0] (str): Identifier for the dataset entry.
@@ -251,14 +252,15 @@ def generate_mat_file(model_path):
         filtered_data = filtfilt(b, a, raw_data)
         peak_indices, _ = find_peaks(x=filtered_data, height= params[3], distance=10)
 
-        plt.figure(figsize=(10, 4))
-        plt.plot(raw_data, label='Raw Data')
-        plt.plot(filtered_data, label='Filtered Data')
-        plt.plot(peak_indices, filtered_data[peak_indices], 'go', label='Peaks')
-        plt.title(f'{params[0]} Data with Peaks')
-        plt.xlabel('Time')
-        plt.legend()
-        plt.show()
+        if show_graph:
+            plt.figure(figsize=(10, 4))
+            plt.plot(raw_data, label='Raw Data')
+            plt.plot(filtered_data, label='Filtered Data')
+            plt.plot(peak_indices, filtered_data[peak_indices], 'go', label='Peaks')
+            plt.title(f'{params[0]} Data with Peaks')
+            plt.xlabel('Time')
+            plt.legend()
+            plt.show()
 
         print(f"For {params[0]}Number of peaks: {len(peak_indices)}")
         spio.savemat(f'predicted/{params[0]}.mat', {'Index': peak_indices})
@@ -292,10 +294,11 @@ if __name__ == '__main__':
 
     class_training(raw_data = raw_data, filtered_data=filtered_data, window_size = window_size, 
                    peak_types = peak_types, peak_indices=peak_indices, 
-                   model_save_path= 'filtered_peak_type_model_12_12_2024.keras')
+                   model_save_path= 'filtered_peak_type_model_12_12_2024.keras',
+                   show_graph=False)
     
      ### For Creating the mat files ######################
-    generate_mat_file(model_path='filtered_peak_type_model_12_12_2024.keras')
+    generate_mat_file(model_path='filtered_peak_type_model_12_12_2024.keras', show_graph=False)
 
 
 
